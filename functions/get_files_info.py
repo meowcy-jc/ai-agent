482a4e721c959a785ed5bfaf803af6f2df7723ae
meowcy-jc <joyce.chen.personal@gmail.com>
2025-11-25 00:06:19 -0500

    made a loop can call agent again and again

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,7 @@
 import os
+from google import genai
+from google.genai import types
+from functions.config import MAX_CHARS
 
 
 # get files infor in a directory
@@ -20,17 +23,17 @@ def get_files_info(working_directory, directory="."):
             new_list = []
 
 # get file size and check if it is a directory
-            for file in list_content:
-                item_path = os.path.join(absolute_path_directory, file)
+            for item in list_content:
+                item_path = os.path.join(absolute_path_directory, item)
                 if os.path.isdir(item_path):
                     file_size = os.path.getsize(item_path)
-                    new_list.append(f"- {file}: file_size={file_size} bytes, is_dir=True")
+                    new_list.append(f"- {item}: file_size={file_size} bytes, is_dir=True")
                                 
                 elif os.path.isfile(item_path):
                     file_size = os.path.getsize(item_path)
-                    new_list.append(f"- {file}: file_size={file_size} bytes, is_dir=False")
+                    new_list.append(f"- {item}: file_size={file_size} bytes, is_dir=False")
 
-# reforamte files infor
+# reformate files infor
             formatted_content = "\n".join(new_list)   
 
             return formatted_content
@@ -38,4 +41,93 @@ def get_files_info(working_directory, directory="."):
         except Exception as e:
             return f"Error: {e}"    
 
-        
+
+# build schema for get_files_infor   
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+
+# build schema for get_file_content
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+
+# build schema for run_python_file
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file within the working directory and returns the output from the interpreter.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
+
+
+
+#build schema for write_file
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes content to a file within the working directory. Creates the file if it doesn't exist.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to write to the file",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
+
+
+# create a list of all the available functions
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+    ]
+)
--- main.py
+++ main.py
@@ -1,44 +1,152 @@
 import os
-from dotenv import load_dotenv
-from google import genai
 import sys
+from google import genai
 from google.genai import types
+from dotenv import load_dotenv
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+from functions.get_files_info import available_functions, get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+from functions.config import *
 
-client = genai.Client(api_key=api_key)
 
+def main():
+    load_dotenv()
 
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
 
-def ask_question(user_prompt: str):
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
     messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+        
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
+    
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response.response
+        ):
+            raise Exception("empty function call result")
+
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        function_responses.append(function_call_result.parts[0])
+        
+    if not function_responses:
+            raise Exception("no function responses generated, exiting.")
+    
+    user_content = types.Content(
+        role="user",
+        parts = function_responses
     )
 
-    if "--verbose" in sys.argv:
-        prompt_tokens = response.usage_metadata.prompt_token_count
-        response_tokens = response.usage_metadata.candidates_token_count
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
+    messages.append(user_content)
 
-    print(response.text)
-
-
-def get_answer():
-    if len(sys.argv) < 2:
-        print("Usage: uv ran main.py <question>")
-        sys.exit(1)
+#call one of the four functions
+def call_function(function_call_part, verbose=False):
+    if verbose is True:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
-        ask_question(sys.argv[1])
+        print(f"Calling function: {function_call_part.name}")
 
-get_answer()
+    function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content":get_file_content,
+    "run_python_file":run_python_file,
+    "write_file":write_file
+    }
+    
 
+    function_name = function_call_part.name
+
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
+    function = function_map[function_name]
+
+    args = dict(function_call_part.args)
+    args["working_directory"] = WORKING_DIR
+
+    function_result = function(**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
